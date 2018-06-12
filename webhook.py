9817de4871f74f00eea562b3a2c9a1a110040b0c
Passive14 <40204510+Passive14@users.noreply.github.com>
2018-06-12 16:23:11 +0200

    update

--- webhook.py
+++ webhook.py
@@ -7,15 +7,6 @@ from flask import request
 from flask import make_response
 
 # Flask app should start in global layout
-import json
-import os
-import requests
-
-from flask import Flask
-from flask import request
-from flask import make_response
-
-# Flask app should start in global layout
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
@@ -58,9 +49,6 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
-
-
-
 
 
 
