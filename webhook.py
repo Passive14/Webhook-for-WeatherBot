ad3261d4b0a09dc7b4428489ad757bedc01eea29
Passive14 <40204510+Passive14@users.noreply.github.com>
2018-06-25 09:51:04 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -14,7 +14,7 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
     
-    res = processRequest(req)
+    res = makeResponse(req)
     
     res = json.dumps(res, indent=4)
     # print(res)
@@ -23,46 +23,24 @@ def webhook():
     return r
 
 def makeResponse(req):
-    if req.get("result").get("action") != "fetchWeatherForecast":
-        return {}
-    result = req.get("result")
+    result = req.get("queryResult")
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
     date = parameters.get("date")
-    if city is None:
-        return None
-    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=06f070197b1f60e55231f8c46658d077')
+    r=requests.get('http://openweathermap.org/data/2.5/forecast?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
     json_object = r.json()
     weather=json_object['list']
-    for i in range(0,30):
+    for i in range(0,len(weather)):
         if date in weather[i]['dt_txt']:
-            condition= weather[i]['weather'][0]['description']
             break
+    condition= weather[i]['weather'][0]['description']
     speech = "The forecast for"+city+ "for "+date+" is "+condition
     return {
-    "speech": speech,
-    "displayText": speech,
-    "source": "apiai-weather-webhook"
+    "fulfillmentText": speech
+    #"source": "apiai-weather-webhook"
     }
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
